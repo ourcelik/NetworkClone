044b54a5588d533a2fc546826ba6d42a8d2caa9e
Onur Çelik <ourcelik@icloud.com>
2021-10-23 20:53:36 +0300

    global-css added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fas);
+import  './assets/style.css';
 
 import { dom } from "@fortawesome/fontawesome-svg-core";
 dom.watch();

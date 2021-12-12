28463cf874d8e258cf9bc6b566b72851f9f5a35b
Onur Çelik <ourcelik@icloud.com>
2021-12-12 13:52:39 +0300

    subheader menu component

--- client/src/components/Shared/Header/headerMenu.vue
+++ client/src/components/Shared/Header/headerMenu.vue
@@ -1,46 +1,218 @@
 <template>
-   <div>
-        <div class="container my-1 " id="menu">
-                    <nav class="d-flex justify-content-around px-5 mx-5">
-                        <a
-                            v-for="category in categories"
-                            :key="category.id"
-                            class="black-color fs-6 p-0 pb-1 text-decoration-none menu-link dropdown"
-                            href="#"
-                        >
-                            <span class="fs-7">{{ category.name }}</span>
-                        </a>
-                         <div class="dropdown-content p-0  animate__animated animate__fadeIn" style="left:0;">
-                <a href="# " class="d-flex p-0">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-                <a href="#">Link 3</a>
- 
-                </div>
-                    </nav>
-                    
-                   
-</div>
-
-   </div>
+    <div>
+        <div class="container my-1" id="menu">
+            <nav class="d-flex justify-content-around px-5 mx-5">
+                <a
+                    v-for="category in categories"
+                    :key="category.id"
+                    class="black-color fs-6 p-0 pb-1 text-decoration-none menu-link dropdown"
+                    href="#"
+                    @mouseover="showSubMenu(category)"
+                >
+                    <span class="fs-7">{{ category.name }}</span>
+                </a>
+                <header-submenu
+                    v-if="subtitles.length > 0"
+                    :subtitles="subtitles"
+                    :photo="currentCategoryPhoto"
+                />
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script>
+import HeaderSubmenu from "./headerSubmenu.vue";
 export default {
+    components: { HeaderSubmenu },
+
     data() {
         return {
             logo: { id: 0, url: "https://www.network.com.tr/assets/v2/img/Network.jpg", width: "220px" },
             categories: [
-                { id: 0, name: "YENI GELENLER" },
-                { id: 1, name: "KADIN" },
-                { id: 2, name: "ERKEK" },
-                { id: 3, name: "SHOES & BAGS" },
-                { id: 4, name: "DIVARESE" },
-                { id: 5, name: "N-TECH" },
-                { id: 6, name: "OUTLET" },
-            ]
+                {
+                    id: 0, name: "YENI GELENLER",
+                    subtitles: [],
+                    categoryPhoto: undefined
+                },
+                {
+                    id: 1, name: "KADIN",
+                    subtitles: [
+                        {
+                            name: "Üst Giyim",
+                            content: [
+                                { id: 1, name: "Kazak" },
+                                { id: 2, name: "Hırka" },
+                                { id: 3, name: "Ceket" },
+                                { id: 4, name: "Sweatshirt" },
+                                { id: 5, name: "Elbise" },
+                                { id: 6, name: "Gömlek" },
+                                { id: 7, name: "Bluz" },
+                                { id: 8, name: "T-Shirt" },
+                                { id: 9, name: "Tulum" },
+                                { id: 10, name: "Yelek" },
+                                { id: 11, name: "Plaj Giyim" },
+                            ],
+                        },
+                        {
+                            name: "Alt Giyim",
+                            content: [
+                                { id: 1, name: "Pantolon" },
+                                { id: 2, name: "Denim Pantolon" },
+                                { id: 3, name: "Casual Pantolon" },
+                                { id: 4, name: "Etek" },
+                                { id: 5, name: "Tayt" },
+                                { id: 6, name: "Eşofman" },
+                                { id: 7, name: "Şort" },
+                            ],
+                        },
+                        {
+                            name: "Dış Giyim",
+                            content: [
+                                { id: 1, name: "Mont" },
+                                { id: 2, name: "Pardösü" },
+                                { id: 3, name: "Deri Mont" },
+                                { id: 4, name: "Kaban" },
+                                { id: 5, name: "Manto" },
+                            ],
+                        },
+                        {
+                            name: "Ayakkabı",
+                            content: [
+                                { id: 1, name: "Bot" },
+                                { id: 2, name: "Çizme" },
+                                { id: 3, name: "Sneaker" },
+                                { id: 4, name: "Ayakkabı" },
+                                { id: 5, name: "Stiletto" },
+                                { id: 6, name: "Babet" },
+                                { id: 7, name: "Espadril" },
+                                { id: 8, name: "Sandalet" },
+                                { id: 9, name: "Terlik" },
+                            ],
+                        },
+                        {
+                            name: "Aksesuar",
+                            content: [
+                                { id: 1, name: "Çanta" },
+                                { id: 2, name: "Cüzdan" },
+                                { id: 3, name: "El Çantası" },
+                                { id: 4, name: "Atkı-Bere-Eldiven" },
+                                { id: 5, name: "Kemer" },
+                                { id: 6, name: "Takı" },
+                                { id: 7, name: "Valiz" },
+                                { id: 8, name: "Çorap" },
+                            ],
+                        }],
+                    categoryPhoto: "https://img-network.mncdn.com/categoryimages/KADIN_2021092913005438342.jpg"
+                },
+                {
+                    id: 2,
+                    name: "ERKEK", subtitles: [
+                        {
+                            name: "Üst Giyim",
+                            content: [
+                                { id: 1, name: "Kazak" },
+                                { id: 2, name: "Hırka" },
+                                { id: 3, name: "Gömlek" },
+                                { id: 4, name: "Sweatshirt" },
+                                { id: 5, name: "Yelek" },
+                                { id: 6, name: "Polo Yaka T-shirt" },
+                                { id: 7, name: "T-Shirt" },
+                                { id: 8, name: "Ceket" },
+                                { id: 9, name: "Takım Elbise" },
+                                { id: 10, name: "Smokin Takım Elbise" },
+                            ],
+                        },
+                        {
+                            name: "Alt Giyim",
+                            content: [
+                                { id: 1, name: "Pantolon" },
+                                { id: 2, name: "Denim Pantolon" },
+                                { id: 3, name: "Casual Pantolon" },
+                                { id: 4, name: "Şort" },
+                                { id: 5, name: "Eşofman" },
+                                { id: 6, name: "Play Giyim" },
+                            ],
+                        },
+                        {
+                            name: "Dış Giyim",
+                            content: [
+                                { id: 1, name: "Palto" },
+                                { id: 2, name: "Mont" },
+                                { id: 3, name: "Deri Mont" },
+                                { id: 4, name: "Kaban" },
+                                { id: 4, name: "Pardösü" },
+                            ],
+                        },
+                        {
+                            name: "Ayakkabı",
+                            content: [
+                                { id: 1, name: "Bot" },
+                                { id: 2, name: "Ayakkabı" },
+                                { id: 3, name: "Sneaker" },
+                                { id: 4, name: "Loafer" },
+                            ],
+                        },
+                        {
+                            name: "Aksesuar",
+                            content: [
+                                { id: 1, name: "Çanta-Cüzdan" },
+                                { id: 2, name: "Kravat-Papyon" },
+                                { id: 3, name: "Mendil" },
+                                { id: 4, name: "Valiz" },
+                                { id: 5, name: "Aksesuar" },
+                                { id: 6, name: "Atkı-Bere-Eldiven" },
+                                { id: 7, name: "Çorap" },
+                                { id: 8, name: "Anahtarlık" },
+                            ],
+                        }],
+                    categoryPhoto: "https://img-network.mncdn.com/categoryimages/ERKEK_2021092913013454444.jpg",
+                },
+
+                {
+                    id: 3, name: "LOOKBOOK",
+                    subtitles: [],
+                    categoryPhoto: undefined
+                },
+                {
+                    id: 4, name: "YILBAŞI",
+                    subtitles: [],
+                    categoryPhoto: undefined
+
+                },
+                {
+                    id: 5, name: "OUTLET",
+                    subtitles: [{
+                        name: "Erkek",
+                        content:[
+                            {id:1,name:"Giyim"},
+                            {id:2,name:"Ayakkabı"},
+                            {id:3,name:"Aksesuar"},
+                        ]
+                    },
+                    {
+                        name: "Kadın",
+                        content:[
+                            {id:1,name:"Giyim"},
+                            {id:2,name:"Ayakkabı"},
+                            {id:3,name:"Aksesuar"},
+                        ]
+                    }],
+                    categoryPhoto: undefined
+                },
+            ],
+            subtitles: [],
+            currentCategoryPhoto: undefined
         };
     },
+    methods: {
+        showSubMenu(category) {
+            this.subtitles = category.subtitles;
+            this.currentCategoryPhoto = category.categoryPhoto;
+
+        },
+    },
+
 }
 </script>
 
@@ -48,20 +220,19 @@ export default {
 .black-color {
     color: black;
 }
-.bg-white{
-    background-color:white ;
+.bg-white {
+    background-color: white;
 }
 
 .fs-7 {
     font-size: small;
     letter-spacing: 1px;
 }
-.menu-link{
+.menu-link {
     transition: 0.1s all ease-in-out;
     border-bottom: 3px solid white;
-
 }
-.menu-link:hover{
+.menu-link:hover {
     border-bottom: 3px solid black;
     color: black;
 }
---
+++ client/src/components/Shared/Header/headerSubmenu.vue
@@ -0,0 +1,40 @@
+<template>
+     <div 
+                    class="dropdown-content pt-5 pb-3 animate__animated animate__fadeIn text-start"
+                    style="left:0;"
+                >
+                    <div class="custom-container row">
+                        <div class="col-2 " v-for="(item, index) in subtitles" :key="index">
+                            <p class="fw-bold" >{{item.name}}</p>
+                            <p class="subItemLink font-title" v-for="(subItem) in item.content" :key="subItem.id">{{subItem.name}}</p>
+                            
+                        </div>
+                       
+                        <div class="col-2">
+                            <img v-if="photo != undefined" class="img-fluid" :src="photo" alt="">
+                        </div>
+                    </div>
+                    
+                </div>
+        
+    
+</template>
+
+<script>
+
+export default{
+    props:["subtitles","photo"],
+    
+}
+</script>
+
+<style>
+.subItemLink{
+    cursor: pointer;
+    transition: all .1s ease-in-out;
+}
+.subItemLink:hover{
+    color: #a5a5a5;
+}
+
+</style>
